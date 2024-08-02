bdb87e35f8eee313be72b69f7fc09ee2cabcacb4
kunal <kishujaiswal3@gmail.com>
2024-08-02 23:55:53 +0530

    backend env added

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:7000/api/v1";
+const BASE_URL = import.meta.env.BACKEND_URL || "http://localhost:7000/api/v1";
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
 export const getUser = async (clerkId) => {
