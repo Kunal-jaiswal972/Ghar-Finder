826534b147e69ab59506638e18a75c35fac9aebc
kunal <kishujaiswal3@gmail.com>
2024-08-04 15:00:37 +0530

    Base url changed

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:7000/api/v1";
+  (import.meta.env.VITE_BACKEND_URL || "http://localhost:7000") + "/api/v1";
+  console.log(BASE_URL)
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
 export const getUser = async (clerkId) => {
